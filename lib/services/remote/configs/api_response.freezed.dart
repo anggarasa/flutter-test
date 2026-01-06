e3630ca798f2991cf2e10d970bd3e7c56c5694cd
anggarasa <anggasaputra6259@gmail.com>
2026-01-06 10:34:51 +0700

    build: update depedencies

--- lib/services/remote/configs/api_response.freezed.dart
+++ lib/services/remote/configs/api_response.freezed.dart
@@ -262,12 +262,12 @@ $ErrorCopyWith<T, Error<T>> get copyWith => _$ErrorCopyWithImpl<T, Error<T>>(thi
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is Error<T>&&const DeepCollectionEquality().equals(other.error, error));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Error<T>&&(identical(other.error, error) || other.error == error));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(error));
+int get hashCode => Object.hash(runtimeType,error);
 
 @override
 String toString() {
@@ -286,7 +286,7 @@ $Res call({
 });
 
 
-
+$NetworkExceptionsCopyWith<dynamic, $Res> get error;
 
 }
 /// @nodoc
@@ -299,14 +299,23 @@ class _$ErrorCopyWithImpl<T,$Res>
 
 /// Create a copy of ApiResponse
 /// with the given fields replaced by the non-null parameter values.
-@pragma('vm:prefer-inline') $Res call({Object? error = freezed,}) {
+@pragma('vm:prefer-inline') $Res call({Object? error = null,}) {
   return _then(Error<T>(
-freezed == error ? _self.error : error // ignore: cast_nullable_to_non_nullable
+null == error ? _self.error : error // ignore: cast_nullable_to_non_nullable
 as NetworkExceptions,
   ));
 }
 
-
+/// Create a copy of ApiResponse
+/// with the given fields replaced by the non-null parameter values.
+@override
+@pragma('vm:prefer-inline')
+$NetworkExceptionsCopyWith<dynamic, $Res> get error {
+  
+  return $NetworkExceptionsCopyWith<dynamic, $Res>(_self.error, (value) {
+    return _then(_self.copyWith(error: value));
+  });
+}
 }
 
 // dart format on
